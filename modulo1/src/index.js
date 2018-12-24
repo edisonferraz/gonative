67cf9ee7deefd141edeef7c3e30fcac9de396a82
Edison Ferraz <edisonferraz@gmail.com>
2018-12-24 16:21:10 +0100

    add reactotron

--- modulo1/src/index.js
+++ modulo1/src/index.js
@@ -1,29 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import "./config/ReactotronConfig";
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, Button } from "react-native";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import Todo from "./components/Todo";
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  state = {
+    todos: [
+      { id: 0, text: "fazer cafe www2" },
+      { id: 1, text: "estudar gonative" }
+    ]
+  };
+
+  addTodo = () => {
+    this.setState({
+      todos: [...this.state.todos, { id: Math.random(), text: "estudar js" }]
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native! www</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {this.state.todos.map(todo => (
+          <Todo key={todo.id} title={todo.text} />
+        ))}
+        <Button title="adicionar todo" onPress={this.addTodo} />
       </View>
     );
   }
@@ -35,15 +37,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
   }
 });
