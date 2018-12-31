2c544afa36e54d7a59ebee2d9f2124704d9808cb
Edison Ferraz <edisonferraz@gmail.com>
2018-12-31 17:36:43 +0100

    Favorito duplicado

--- modulo3/src/store/sagas/favorites.js
+++ modulo3/src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import api from '../../services/api';
 
 import { addFavoriteSuccess, addFavoriteFailure } from '~/store/actions/favorites';
@@ -7,7 +7,14 @@ export function* addFavoriteRequest(action) {
   try {
     const response = yield call(api.get, `/repos/${action.payload.repoName}`);
 
-    yield put(addFavoriteSuccess(response.data));
+    const favorites = yield select(state => state.favorites.data);
+    const isDuplicated = favorites.find(favorite => favorite.id === response.data.id);
+
+    if (isDuplicated) {
+      yield put(addFavoriteFailure('Repositório duplicado'));
+    } else {
+      yield put(addFavoriteSuccess(response.data));
+    }
   } catch (error) {
     yield put(addFavoriteFailure('Repositório não existe'));
   }
