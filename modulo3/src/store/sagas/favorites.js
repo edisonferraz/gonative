4bd1c089d7caf9ba4db3a66fd0bfa95787458f2b
Edison Ferraz <edisonferraz@gmail.com>
2018-12-31 17:50:10 +0100

    Duck Pattern

--- modulo3/src/store/sagas/favorites.js
+++ modulo3/src/store/sagas/favorites.js
@@ -1,7 +1,7 @@
 import { call, put, select } from 'redux-saga/effects';
 import api from '../../services/api';
 
-import { addFavoriteSuccess, addFavoriteFailure } from '~/store/actions/favorites';
+import { Creators as FavoriteActions } from '~/store/ducks/favorites';
 
 export function* addFavoriteRequest(action) {
   try {
@@ -11,11 +11,11 @@ export function* addFavoriteRequest(action) {
     const isDuplicated = favorites.find(favorite => favorite.id === response.data.id);
 
     if (isDuplicated) {
-      yield put(addFavoriteFailure('Repositório duplicado'));
+      yield put(FavoriteActions.addFavoriteFailure('Repositório duplicado'));
     } else {
-      yield put(addFavoriteSuccess(response.data));
+      yield put(FavoriteActions.addFavoriteSuccess(response.data));
     }
   } catch (error) {
-    yield put(addFavoriteFailure('Repositório não existe'));
+    yield put(FavoriteActions.addFavoriteFailure('Repositório não existe'));
   }
 }
